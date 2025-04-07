8e4875733844f44cd0a95b2c0a89a0202a6901a7
Muzamil <sk.muzamil1018@email.com>
2025-04-07 19:36:27 +0530

    my fouth commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputBox=doc.getElementById("input-box");
+const listContainer=doc.getElementById("list-container");
+function addTask(){
+    if(inputBox.value===''){
+        alert("You must write something!");
+    }else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData(); 
+}
